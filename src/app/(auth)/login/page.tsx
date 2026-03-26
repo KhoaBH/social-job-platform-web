1216d553044c93a9dbcbbf19d59b018dd6a7c6ea
KhoaBH <lamanhkhoa2004@gmail.com>
2026-03-26 15:22:27 +0700

    testgit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "@/components/auth/login-form";
+import LoginForm from "@/features/auth/components/login-form";
 
 export default function LoginPage() {
   return (
